e6a7b137703fb5ee55340973388a44e392a8fc46
utkarsh.s1 <utkarsh.s1@iimjobs.com>
2024-08-26 13:17:14 +0530

    first_commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/usib', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
